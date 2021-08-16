739ad6caa7bf608b188242d16f50834e640761a7
mimamm <34913510+mimamm@users.noreply.github.com>
2021-08-16 07:16:54 +0700

    Update AddNew.js

--- src/views/posts/add-new/AddNew.js
+++ src/views/posts/add-new/AddNew.js
@@ -36,7 +36,7 @@ const AddNew = () => {
         if (category.length < 3 && category.length !== 0) setCategoryValid(false)
         else setCategoryValid(true)
 
-        if (title.length > 20 && content.length > 200 && category.length > 3) setButtonValid(true)
+        if (title.length >= 20 && content.length >= 200 && category.length >= 3) setButtonValid(true)
         else setButtonValid(false)
     })
 
